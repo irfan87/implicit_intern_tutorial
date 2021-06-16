44ac742de550e96c7d20e3c4d5cb03bfc8407064
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-06-16 16:46:15 +0800

    use new ways for activity result that Google's provided

--- app/src/main/java/com/irfanshukri203/implicitinterntutorial/MainActivity.kt
+++ app/src/main/java/com/irfanshukri203/implicitinterntutorial/MainActivity.kt
@@ -2,10 +2,20 @@ package com.irfanshukri203.implicitinterntutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTakePhoto.setOnClickListener {
+            getContent.launch("image/*")
+        }
+    }
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        ivPhoto.setImageURI(uri)
     }
 }
